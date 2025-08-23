1e0f5c9f7a6e1c85c2cf329164ee67f76cb08d8f
primal-scream <ryosuke.fujio@gmail.com>
2025-08-23 20:02:42 +0900

    Add window border

--- init.lua
+++ init.lua
@@ -166,6 +166,9 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- winborder settings
+vim.o.winborder = 'rounded'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
